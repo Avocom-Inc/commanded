b04d85ee292b2327e79e4866b28ffca944c26d8c
wyse144 <erick@erickmcarthy.com>
2020-05-22 08:08:11 -0500

    update deps

--- mix.exs
+++ mix.exs
@@ -54,13 +54,13 @@ defmodule Commanded.Mixfile do
       {:elixir_uuid, "~> 1.2"},
 
       # Optional dependencies
-      {:jason, "~> 1.2", optional: true},
+      {:jason, "~> 1.2.1", optional: true},
       {:phoenix_pubsub, "~> 2.0", optional: true},
 
       # Build and test tools
       {:benchfella, "~> 0.3", only: :bench},
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.21", only: :dev},
+      {:ex_doc, "~> 0.22.1", only: :dev},
       {:mix_test_watch, "~> 1.0", only: :dev},
       {:mox, "~> 0.5", only: [:bench, :test]}
     ]
